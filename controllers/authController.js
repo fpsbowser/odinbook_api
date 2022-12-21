55d9aefd244f3e40b6c7b5621cad4da4000af3d4
Dakota <36649413+fpsbowser@users.noreply.github.com>
2022-12-20 20:14:27 -0500

    removed post_login, added user email verification

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,47 +1,12 @@
 const User = require('../models/user');
 const async = require('async');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const jwtStrategy = require('passport-jwt').Strategy;
+
 const { body, validationResult } = require('express-validator');
-
-exports.post_login = [
-  body('email', 'Must provide email').trim().isLength({ min: 1 }).escape(),
-  body('password', 'Must provide password')
-    .trim()
-    .isLength({ min: 1 })
-    .escape(),
-  async (req, res, next) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      console.log('validation errors');
-      return res.json(errors).status(400);
-    } else {
-      // Search for user
-      try {
-        const user = await User.findOne({ email: req.body.email }).exec();
-        if (user) {
-          // USER FOUND : Compare passwords
-          let passwordIsValid = bcrypt.compareSync(
-            req.body.password,
-            user.password
-          );
-          if (!passwordIsValid) {
-            return res.json({ message: 'Invalid Password' }).status(401);
-          } else {
-            // TODO: Login User - PASSPORT
-            return res.json({ message: 'Successfully login' });
-          }
-        } else {
-          // user email not found
-          return res.json({ message: 'User email not found.' }).status(404);
-        }
-      } catch (err) {
-        console.log(err);
-        return res.json(err);
-      }
-    }
-  },
-];
 
 exports.post_signup = [
   body('email', 'must provide email')
@@ -75,27 +40,87 @@ exports.post_signup = [
     if (!errors.isEmpty()) {
       return res.json({ message: 'Validation error', errors }).status(400);
     }
-    // Create new user
-    const user = new User({
-      name: {
-        first: req.body.firstname,
-        last: req.body.lastname,
-      },
-      // TODO: Add dob validation
-      dob: null,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, 8),
-      friends: [],
-      friend_requests: [],
-      posts: [],
-    });
-
-    user.save((err, user) => {
+    // Check if email exists in DB
+    User.exists({ email: req.body.email }, (err, userExists) => {
       if (err) {
-        return res.json(err).status(500);
+        return res.json(err);
+      }
+      if (userExists) {
+        return res
+          .json({
+            error:
+              'The email you have entered already exists! Please use a different email.',
+          })
+          .status(400);
       } else {
-        res.json({ message: 'Successfully registered user', user });
+        // Create new user
+        const user = new User({
+          name: {
+            first: req.body.firstname,
+            last: req.body.lastname,
+          },
+          // TODO: Add dob validation
+          dob: null,
+          email: req.body.email,
+          password: bcrypt.hashSync(req.body.password, 8),
+          friends: [],
+          friend_requests: [],
+          posts: [],
+        });
+        user.save((err, user) => {
+          if (err) {
+            return res.json(err).status(500);
+          } else {
+            res.json({
+              message: 'Successfully registered user',
+              user: {
+                name: user.name,
+                email: user.email,
+                id: user._id,
+              },
+            });
+          }
+        });
       }
     });
   },
 ];
+
+// exports.post_login = [
+//   body('email', 'Must provide email').trim().isLength({ min: 1 }).escape(),
+//   body('password', 'Must provide password')
+//     .trim()
+//     .isLength({ min: 1 })
+//     .escape(),
+//   async (req, res, next) => {
+//     const errors = validationResult(req);
+
+//     if (!errors.isEmpty()) {
+//       console.log('validation errors');
+//       return res.json(errors).status(400);
+//     } else {
+//       // Search for user
+//       try {
+//         const user = await User.findOne({ email: req.body.email }).exec();
+//         if (user) {
+//           // USER FOUND : Compare passwords
+//           let passwordIsValid = bcrypt.compareSync(
+//             req.body.password,
+//             user.password
+//           );
+//           if (!passwordIsValid) {
+//             return res.json({ message: 'Invalid Password' }).status(401);
+//           } else {
+//             // TODO: Login User - PASSPORT
+//           }
+//         } else {
+//           // user email not found
+//           return res.json({ message: 'User email not found.' }).status(401);
+//         }
+//       } catch (err) {
+//         console.log(err);
+//         return res.json(err);
+//       }
+//     }
+//   },
+// ];
